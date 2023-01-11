f0b1522a83bcfadbc3d0b04894a0bd68e087007d
volodymyrshevchenko <Chief_94@ukr.net>
2023-01-11 15:06:35 +0200

    Done

---
+++ src/Hw13/Main.java
@@ -0,0 +1,17 @@
+package Hw13;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Person person = new Person("Vova","Shevchenko",28 ,"Male","Ukraine");
+        person.firstName = "Vova";
+        person.lastName = "Shevchenko";
+        person.age = 28;
+        person.gender = "Male";
+        person.address = "Ukraine";
+
+
+
+
+    }
+}
